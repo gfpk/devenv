f21eb101a4708c35a3d10505d16d48c99a8ed666
Greg Filipek <kongu3333@gmail.com>
2018-03-26 15:17:05 +0100

    mod 11 done

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -1,22 +1,19 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
-import webpack from 'webpack';
-import config from '../webpack.config.dev';
+import compression from 'compression';
+
 
 /*eslint-disable no-console*/
 
 const port = 8080;
 const app = express();
-const compiler  = webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler, {
-	noInfo:true,
-	publicPath:config.output.publicPath
-}));
+app.use(compression());
+app.use(express.static('dist')); // to serve static files
 
 app.get('/', function(req, res){
-	res.sendFile(path.join(__dirname,'../src/index.html'))
+	res.sendFile(path.join(__dirname,'../dist/index.html'))
 })
 
 app.get('/users', function(req, res){
@@ -37,5 +34,3 @@ app.listen(port, function(err){
 		open('http://localhost:'+port)
 	}
 })
-
-
---
+++ src/vendor.js
@@ -0,0 +1,4 @@
+/* to illustrate bundle splitting, this lib will not change when
+updating just our code. It will be kept in browser chache */
+
+import fetch from 'whatwg-fetch';
